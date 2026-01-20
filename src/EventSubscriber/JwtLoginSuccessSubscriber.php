e1b518084c0c364ddac5a5fae921b0a21dae7b2d
Al hussein Khouma/Full Stack Developper <68778997+moustapha96@users.noreply.github.com>
2026-01-20 16:45:24 +0000

    Update JwtLoginSuccessSubscriber.php

--- src/EventSubscriber/JwtLoginSuccessSubscriber.php
+++ src/EventSubscriber/JwtLoginSuccessSubscriber.php
@@ -4,23 +4,17 @@ declare(strict_types=1);
 
 namespace App\EventSubscriber;
 
-use App\Entity\RefreshToken;
-use Doctrine\ORM\EntityManagerInterface;
-use Gesdinet\JWTRefreshTokenBundle\Model\RefreshTokenManagerInterface;
 use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class JwtLoginSuccessSubscriber implements EventSubscriberInterface
 {
     public function __construct(
-        private RefreshTokenManagerInterface $refreshTokenManager,
-        private EntityManagerInterface $em
     ) {}
 
     public static function getSubscribedEvents(): array
     {
         return [
-            'lexik_jwt_authentication.on_authentication_success' => 'onAuthenticationSuccess',
         ];
     }
 
